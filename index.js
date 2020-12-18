a64e0796c1016e99f2d93c438f5004918f04ab2c
triagungtio07 <triagungtio07@gmail.com>
2020-12-18 14:57:09 +0700

    crud

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get('/', function(req, res){
   res.send('Hello My name is TRI_AGUNG_PRASETIO');
 });
 
+app.post('/', function(req, res){
+  res.send('Hello My name is TRI_AGUNG_PRASETIO but it from post');
+});
+
+app.put('/', function(req, res){
+  res.send('Hello My name is TRI_AGUNG_PRASETIO but it from put');
+});
+
+app.delete('/', function(req, res){
+  res.send('Hello My name is TRI_AGUNG_PRASETIO but it from delete');
+});
+
+
+
+
 app.listen(port, function(){
   console.log('Hello my apps is running on port '+port);
 });
